17f10b10de20c5df54bcbd78842f13171eea0c56
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2022-02-21 22:02:54 -0600

    move the code to the main method

--- Prog1.java
+++ Prog1.java
@@ -1,20 +1,6 @@
 package CS390Assignment1;
 
 public class Prog1 {
-    /**
-     * Problem 1
-     */
-    public static void computeRandomNumber(){
-
-        //assigning x and y to a random number;
-        int x = RandomNumbers.getRandomInt(1,9);
-        int y = RandomNumbers.getRandomInt(3,14);
-
-        //printing out the result
-        System.out.println(Math.pow(Math.PI,x));
-        System.out.println(Math.pow(y,Math.PI));
-
-    }
 
     /**
      *Problem 2
@@ -22,7 +8,16 @@ public class Prog1 {
 
     public static void main(String[] args) {
 
-   computeRandomNumber();
+        /**
+         * Problem 1
+         */
+        //assigning x and y to a random number;
+        int x = RandomNumbers.getRandomInt(1,9);
+        int y = RandomNumbers.getRandomInt(3,14);
+
+        //printing out the result
+        System.out.println(Math.pow(Math.PI,x));
+        System.out.println(Math.pow(y,Math.PI));
 
 
     }
