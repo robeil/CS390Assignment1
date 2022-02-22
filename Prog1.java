4c432e43427f50972020cae5beaf98f5990d8be9
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2022-02-21 21:49:51 -0600

    solved problem 1

--- Prog1.java
+++ Prog1.java
@@ -1,7 +1,29 @@
-package lesoon1Assignment.assignment1;
+package CS390Assignment1;
 
 public class Prog1 {
+    /**
+     * Problem 1
+     */
+    public static void computeRandomNumber(){
+
+        //assigning x and y to a random number;
+        int x = RandomNumbers.getRandomInt(1,9);
+        int y = RandomNumbers.getRandomInt(3,14);
+
+        //printing out the result
+        System.out.println(Math.pow(Math.PI,x));
+        System.out.println(Math.pow(y,Math.PI));
+
+    }
+
+    /**
+     *Problem 2
+     */
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+   computeRandomNumber();
+
+
     }
 }
